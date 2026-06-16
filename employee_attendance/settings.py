99854d30618a94d1adf14de33038448f42383c74
Renil197 <renilc761@gmail.com>
2026-06-16 12:29:43 +0530

    Fix environment and dependencies

--- employee_attendance/settings.py
+++ employee_attendance/settings.py
@@ -13,12 +13,15 @@ import environ
 import os
 from pathlib import Path
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 env = environ.Env(
     DEBUG=(bool, False)
 )
 environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
+import pymysql
+pymysql.install_as_MySQLdb()
 
 
 # Quick-start development settings - unsuitable for production
